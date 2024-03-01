cd6e52fb24ced9aca2212c7829cd418060811ba5
Rafael Rangel <rafrngel@gmail.com>
2024-03-01 00:15:57 -0300

    added cms

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/contact.css')
   eleventyConfig.addPassthroughCopy('src/participe.css')
   eleventyConfig.addPassthroughCopy('src/assets')
+  eleventyConfig.addPassthroughCopy('src/admin')
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
